af21fd3dcc527889dc31eba1ad7fcdd579835020
ovkerod <ovkerod@yahoo.com>
2025-05-13 19:35:56 -0400

    tiles know if they have an adjacent tile

--- test_ground.py
+++ test_ground.py
@@ -1,15 +1,2 @@
-class Item:
-    def __init__(self):
-        self.value = 1
-
-
-item = Item()
-
-item_list = []
-item_list_2 = []
-item_list.append(item)
-item_list_2.append(item)
-
-item_list[0].value = 2
-
-print(item_list_2[0].value)
+my_tuple = (1, 2) + (3, 4)
+print(my_tuple)
