9f9f07ac06b04b2cd35ef77fbc014b6d721d6936
ovkerod <ovkerod@yahoo.com>
2025-05-24 11:20:11 -0400

    plots know what plots are adjacent to them

--- test_ground.py
+++ test_ground.py
@@ -1,11 +1,13 @@
-row = 1
+row = 0
 col = 1
-index_shift = ((int(row % 2 == 1), 1),
+index_shift = ((int(col % 2 == 1), 1),
             (1, 0),
-            (1*int(row % 2 == 1), -1),
-            (-1*int(row % 2 == 0), -1),
+            (1*int(col % 2 == 1), -1),
+            (-1*int(col % 2 == 0), -1),
             (-1, 0),
-            (-1*int(row % 2 == 0), 1)
+            (-1*int(col % 2 == 0), 1)
 )
 
 print(index_shift)
+
+my_list = [(1, 1), (1, 1)]
